2ec4f4300ca01007020a033ed96df5b77a093d31
Bautista <bautistabertolami@gmail.com>
2020-04-04 16:28:38 -0400

    using create react app

--- src/App.js
+++ src/App.js
@@ -1,44 +1,26 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import CreateTodo from "./components/create-todo.component";
-import EditTodo from "./components/edit-todo.component";
-import TodosList from "./components/todos-list.component";
-import SignUp from "./components/signup";
-
-import logo from "./LogoV2.png";
-
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="UnitedPlanet" target="_blank">
-              <img src={logo} width="30" height="30" alt="UnitedPlanet" />
-            </a>
-            <Link to="/signup" className="navbar-brand">UnitedPlanet</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/signup" className="nav-link">Create New Account</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Sign In</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br/>
-          <Route path="/" exact component={TodosList} />
-          <Route path="/edit/:id" component={EditTodo} />
-          <Route path="/create" component={CreateTodo} />
-          <Route path="/signup" component={SignUp} />
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
